b072433b327cf531a7090482a6d370a65f1de14f
Oleh Dmytrychenko <dmytrychenko.oleh@gmail.com>
2023-08-07 09:42:15 +0300

    leverage existing plugin registration

--- docker.sh
+++ docker.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-CD=$(dirname $BASH_SOURCE)
-
-cd $CD
-CD=$(pwd)
-cd $CD/docker
+DIR=$(dirname "$(readlink -f "$0")")
 
 #docker-compose up --build
 docker build ./ -t steamvr_openhmd/build
 docker run -ti --rm \
     -e USER=$USER \
     -v $HOME:/home/$USER \
-    -v $CD:/tmp/dev/ \
+    -v $DIR:/tmp/dev/ \
     -v /etc/passwd:/etc/passwd \
     --name build \
 steamvr_openhmd/build:latest "$@"
 
-
-# register this build with steamVR automatically
-~/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/vrpathreg adddriver $CD/build/
-
-
-# run steamVR, giving priority to Steam runtime libraries instead of system libraries.
-export STEAM_RUNTIME_PREFER_HOST_LIBRARIES=0
-~/.local/share/Steam/ubuntu12_32/steam-runtime/run.sh ~/.local/share/Steam/steamapps/common/SteamVR/bin/vrstartup.sh
+./register.sh
